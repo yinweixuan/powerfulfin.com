25f052a1d0212490e3f61e1047bed2f6545f86fa
王毅 <wangyi@kezhanwang.cn>
2018-12-22 14:15:44 +0800

    新增订单信息ar类

---
+++ app/Models/ActiveRecord/ARPFLoan.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 2018/12/22
+ * Time: 2:13 PM
+ */
+
+namespace App\Models\ActiveRecord;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ARPFLoan extends Model
+{
+    protected $table = 'pf_loan';
+
+    const TABLE_NAME = 'pf_loan';
+}
---
+++ app/Models/ActiveRecord/ARPFLoanLog.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 2018/12/22
+ * Time: 2:14 PM
+ */
+
+namespace App\Models\ActiveRecord;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ARPFLoanLog extends Model
+{
+    protected $table = 'pf_loan_log';
+
+    const TABLE_NAME = 'pf_loan_log';
+}
