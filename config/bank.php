df70e46b9b6c4072a5897bc210bb10ff4158638b
王毅 <wangyi@kezhanwang.cn>
2018-12-26 16:53:15 +0800

    银行卡logo

--- config/bank.php
+++ config/bank.php
@@ -12,83 +12,83 @@ return [
         'bank_code' => 'ICBC',
         'jcfc_bank_code' => '102100099996',//JCFC 银行code
         'jcfc_bank_code_tl' => '0102', //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/1bafa7b17fd5f1f35f45e39a55eb7cc9.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/icbc.png',
     ),
     'ABC' => array(
         'bankname' => '中国农业银行',
         'bank_code' => 'ABC',
         'jcfc_bank_code' => '103100000026',//JCFC 银行code
         'jcfc_bank_code_tl' => '0103',//JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/eab88d986327bb71be1a2090af618560.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/abc.png',
     ),
     'BOC' => array(
         'bankname' => '中国银行',
         'bank_code' => 'BOC',
         'jcfc_bank_code' => '104100000004',//JCFC 银行code
         'jcfc_bank_code_tl' => '0104',//JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/0daf14aac045858eef05a43f6d2bb10a.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/boc.png',
     ),
     'CCB' => array(
         'bankname' => '中国建设银行',
         'bank_code' => 'CCB',
         'jcfc_bank_code' => '105100000017', //JCFC 银行code
         'jcfc_bank_code_tl' => '0105',  //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/4a7e2ebff428d3f7813e64356b613656.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/ccb.png',
     ),
     'BCOM' => array(
         'bankname' => '交通银行',
         'bank_code' => 'BCOM',
         'jcfc_bank_code' => '',   //JCFC 银行code
         'jcfc_bank_code_tl' => '',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/813a23d568e992bbfd40a4ef521ad943.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/bcom.png',
     ),
     'CITIC' => array(
         'bankname' => '中信银行',
         'bank_code' => 'CITIC',
         'jcfc_bank_code' => '302100011000',   //JCFC 银行code
         'jcfc_bank_code_tl' => '0302',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/7bbd86afdc3bddea0053b4179f1680ce.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/citic.png',
     ),
     'HXB' => array(
         'bankname' => '中国光大银行',
         'bank_code' => 'HXB',
         'jcfc_bank_code' => '303100000006',   //JCFC 银行code
         'jcfc_bank_code_tl' => '0303',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/26ce47077af4877ec7ddbaafa1a05131.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/hxb.png',
     ),
     'GDB' => array(
         'bankname' => '广东发展银行',
         'bank_code' => 'GDB',
         'jcfc_bank_code' => '306581000003',   //JCFC 银行code
         'jcfc_bank_code_tl' => '0306',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/61306de8abf4d1460e0fa6f3f6458e14.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/gdb.png',
     ),
     'PAB' => array(
         'bankname' => '平安银行',
         'bank_code' => 'PAB',
         'jcfc_bank_code' => '307584007998',   //JCFC 银行code
         'jcfc_bank_code_tl' => '0307',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/72f71254098872c24056f9636ddfda74.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/pab.png',
     ),
     'CMB' => array(
         'bankname' => '招商银行',
         'bank_code' => 'CMB',
         'jcfc_bank_code' => '308584000013',//JCFC 银行code 暂不支持
         'jcfc_bank_code_tl' => '0308',//JCFC 对应人行code   暂不支持
-        'logo' => 'http://res1.kezhanwang.cn/static/others/8ec888221f56b4385ac20a43e1b0ac72.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/cmb.png',
     ),
     'CIB' => array(
         'bankname' => '兴业银行',
         'bank_code' => 'CIB',
         'jcfc_bank_code' => '309391000011',   //JCFC 银行code
         'jcfc_bank_code_tl' => '0309',    //JCFC 对应人行code
-        'logo' => 'http://res1.kezhanwang.cn/static/others/7ea1502fdff7663c01c956a931067f93.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/cib.png',
     ),
     'PSBC' => array(
         'bankname' => '中国邮政储蓄银行',
         'bank_code' => 'PSBC',
         'jcfc_bank_code' => '403100000004',//JCFC 银行code 暂不支持
         'jcfc_bank_code_tl' => '0403',//JCFC 对应人行code 暂不支持
-        'logo' => 'http://res1.kezhanwang.cn/static/others/39fea7327b6045bc4af7f15741f40eaa.png',
+        'logo' => 'http://www.powerfulfin.com/img/banks/psbc.png',
     ),
 ];
