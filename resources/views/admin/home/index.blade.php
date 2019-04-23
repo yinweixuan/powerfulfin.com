21045a6245c4aeaecccd45da8c4d0309d0e1ed2c
王毅 <wangyi@kezhanwang.cn>
2019-04-23 15:19:04 +0800

    取消首页访问地图

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,6 +1,4 @@
 <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/Ionicons/css/ionicons.min.css") }}">
-<!-- jvectormap -->
-<link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/jvectormap/jquery-jvectormap.css") }}">
 <div class="row">
     <div class="col-md-3 col-sm-6 col-xs-12">
         <div class="info-box">
@@ -60,58 +58,3 @@
     <!-- /.col -->
 </div>
 <!-- /.row -->
-
-
-<!-- /.row -->
-<div class="row">
-    <!-- Left col -->
-    <div class="col-md-12">
-        <!-- MAP & BOX PANE -->
-        <div class="box box-danger">
-            <div class="box-header with-border">
-                <h3 class="box-title">访问统计</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
-                <div class="row">
-                    <div class="col-md-9 col-sm-8">
-                        <div class="pad">
-                            <!-- Map will be created here -->
-                            <div id="world-map-markers" style="height: 325px;"></div>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-md-3 col-sm-4">
-                        <div class="pad box-pane-right bg-green" style="min-height: 280px">
-                            <div class="description-block margin-bottom">
-                                <div class="sparkbar pad" data-color="#fff">90,70,90,70,75,80,70</div>
-                                <h5 class="description-header">8390</h5>
-                                <span class="description-text">Visits</span>
-                            </div>
-                            <!-- /.description-block -->
-                            <div class="description-block margin-bottom">
-                                <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                                <h5 class="description-header">30%</h5>
-                                <span class="description-text">Referrals</span>
-                            </div>
-                            <!-- /.description-block -->
-                            <div class="description-block">
-                                <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                                <h5 class="description-header">70%</h5>
-                                <span class="description-text">Organic</span>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
-    </div>
-    <!-- /.col -->
-</div>
-{{ \Encore\Admin\Admin::js('/vendor/laravel-admin/jvectormap/jquery-jvectormap-1.2.2.min.js') }}
-{{ \Encore\Admin\Admin::js('/vendor/laravel-admin/jvectormap/jquery-jvectormap-world-mill-en.js') }}
